3fdf4002bcc4201c25b2dae3b226dcce3477d0df
Misha Osinovskiy <misha.osinovskiy@gmail.com>
2020-05-07 07:55:48 -0700

    Price Calc Working

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,31 @@
+$(document).ready(function() {
+    /* If Product Page, attach trust badget next to price */
 
+    var isProduct = $("body.product-template-default");
+    var badge = $(".trust-badges");
+    var productMeta = $(".product_meta");
+    if (isProduct) {
+        $(".trust-badges").addClass("attached-to-prod");
+        $(productMeta).append(badge);
+    }
 
-$(document).ready(function () {
+    function calcSavings() {
+        var theprice = document.querySelector(".single-product .price");
+        if (theprice) {
+            var startPrice = document.querySelector(".price del");
+            startPrice = startPrice.textContent.split("$")[1];
 
-	/* If Product Page, attach trust badget next to price */
+            var discountPrice = document.querySelector(".price ins");
+            discountPrice = discountPrice.textContent.split("$")[1];
 
-	var isProduct = $('body.product-template-default');
-	var badge = $('.trust-badges');
-	var productMeta = $('.product_meta');
-	if (isProduct) {
-		$('.trust-badges').addClass('attached-to-prod');
-		$(productMeta).append(badge);
-	}
+            var savings = startPrice - discountPrice;
+            var priceElement = document.createElement("div");
 
-	// 	function calcSavings(){
-	//   var startPrice = 30;
-	//   var discountPrice = 25;
+            priceElement.classList.add("you-save");
+            priceElement.innerHTML = "You Save $<span>" + savings + "</span>";
+            theprice.append(priceElement);
+        }
+    }
 
-	//   var savings = startPrice - discountPrice;  
-	//   var priceElement = document.createElement('div');
-	//   priceElement.classList.add('you-save');
-	//   priceElement.innerHTML = 'You Save: ' + savings; 
-	//   return priceElement;
-	// }
-
-	// calcSavings();
-
-
+    calcSavings();
 });
-
