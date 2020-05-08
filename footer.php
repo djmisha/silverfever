94ad144988f8d6846f51ce99436866646f410a04
Misha Osinovskiy <misha.osinovskiy@gmail.com>
2020-05-08 16:48:45 -0700

    daily commit

--- footer.php
+++ footer.php
@@ -16,18 +16,15 @@ $url = get_stylesheet_directory_uri();
 
 	<div class="trust-badges">
 		<div class="trust-badge">
-			<img src="<?php echo $url ?>/images/icon-ship.svg" alt="ship">
-			<p> <strong>30-Days No-Questions Return Policy:</strong><br /> If you are not completely satisfied with your purchase, you can claim a 100% refund. Only shipping and delivery costs will be retained.
-			</p>
+			<div class="badge-image"> <img src="<?php echo $url ?>/images/icon-ship.svg" alt="ship"> </div>
+			<div> <p> <strong>30-Days No-Questions Return Policy:</strong><br /> If you are not completely satisfied with your purchase, you can claim a 100% refund. Only shipping and delivery costs will be retained. </p> </div>
 		</div>
 		<div class="trust-badge">
-			<img src="<?php echo $url ?>/images/icon-return.svg" alt="return">
-			<p> <strong>Irrefutable Exchange Policy:</strong><br /> We welcome exchanges, if you prefer an exchange, please choose another color/size/item from our offerings. </p>
-		</div>
+			<div class="badge-image"> <img src="<?php echo $url ?>/images/icon-return.svg" alt="return"> </div>
+			<div> <p> <strong>Irrefutable Exchange Policy:</strong><br /> We welcome exchanges, if you prefer an exchange, please choose another color/size/item from our offerings. </p> </div> </div>
 		<div class="trust-badge">
-			<img src="<?php echo $url ?>/images/icon-ssl.svg" alt="ssl">
-			<p> <strong>Secure Checkout</strong> <br />SSL Enabled Secure Checkout. Shop safely with us. 
-			</p>
+			<div class="badge-image"> <img src="<?php echo $url ?>/images/icon-ssl.svg" alt="ssl"> </div>
+			<div> <p> <strong>Secure Checkout</strong> <br />SSL Enabled Secure Checkout. Shop safely with us. </p> </div>
 		</div>
 	</div>
 
@@ -91,7 +88,7 @@ $url = get_stylesheet_directory_uri();
 ?>
 		
 <script id="__bs_script__">//<![CDATA[
-    document.write("<script async src='http://silverfever.local/browser-sync/browser-sync-client.js?v=2.26.7'><\/script>".replace("HOST", location.hostname));
+    document.write("<script async src='http://HOST:3000/browser-sync/browser-sync-client.js?v=2.26.7'><\/script>".replace("HOST", location.hostname));
 //]]></script>
 
 <? endif; ?>
--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,61 @@
+/* Helper Function */
+
+/* Get Elements on Page */
+function get(element) {
+    element = document.querySelector(element);
+    return element;
+}
+
+/* Create a Div with classes and content */
+// function create(element, cssclass, content) {
+//     element = document.createElement("div");
+//     element.classList.add(cssclass);
+//     element.innerHTML = content;
+//     return element;
+// }
+
 $(document).ready(function() {
-    /* If Product Page, attach trust badget next to price */
-
-    var isProduct = document.querySelector(".single-product");
-    var badge = $(".trust-badges");
-    var productMeta = $(".product_meta");
-
+    /* run Product page Functions */
+    var isProduct = get(".single-product");
     if (isProduct) {
+        moveElements();
+        trustBadges();
+    }
+
+    /* If Product Page, attach trust badget next to price */
+    function trustBadges() {
+        var badge = $(".trust-badges");
+        var productMeta = $(".product_meta");
         $(".trust-badges").addClass("attached-to-prod");
         $(productMeta).append(badge);
+    }
+
+    /* Move Single Product Elements around for Mobile */
+    function moveElements() {
+        width = $(window).width();
+        if (width < 768) {
+            var prodTitle = get(".product_title");
+            var prodSlide = get(".woocommerce-notices-wrapper");
+            prodSlide.prepend(prodTitle);
+
+            var prodSale = get(".onsale");
+            prodSale.classList.add("moved-sale");
+            var prodPrice = get(".price");
+            prodPrice.prepend(prodSale);
+        }
     }
 });
 
 /* Calculate Savings */
 
 function calcSavings() {
-    var theprice = document.querySelector(".single-product .price");
-    var onSale = document.querySelector(".onsale");
+    var theprice = get(".single-product .price");
+    var onSale = get(".single-product .onsale");
     if (onSale) {
-        var startPrice = document.querySelector(".price del");
+        var startPrice = get(".price del");
         startPrice = startPrice.textContent.split("$")[1];
 
-        var discountPrice = document.querySelector(".price ins");
+        var discountPrice = get(".price ins");
         discountPrice = discountPrice.textContent.split("$")[1];
 
         var savings = startPrice - discountPrice;
