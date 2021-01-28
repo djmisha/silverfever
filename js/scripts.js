26bf2ace906005d374cc8270d717dd3c0845c319
Misha Osinvoskiy <misha.osinovskiy@gmail.com>
2021-01-28 08:07:50 -0800

    daily commit

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@
 var nav = priorityNav.init({
     mainNavWrapper: ".primary-navigation", // mainnav wrapper selector (must be direct parent from mainNav)
     mainNav: "#menu-main-navigation", // mainnav selector. (must be inline-block)
-    navDropdownLabel: "&darr;",
+    navDropdownLabel: " More &darr;",
     navDropdownClassName: "nav__dropdown", // class used for the dropdown.
     navDropdownToggleClassName: "nav__dropdown-toggle", // class used for the dropdown toggle.
 });
